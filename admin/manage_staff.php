10ec8c9ce8ffea9ff4770155392b0a2f7487c912
kiki2425 <kikisafitrial@gmail.com>
2025-08-28 09:20:14 +0700

    merge

--- admin/manage_staff.php
+++
@@ -1,8 +0,0 @@
-<?php
-// admin/manage_staff.php
-session_start();
-
-$_SESSION['nama'] = $_SESSION['nama'] ?? 'Guest (Preview)';
-
-// Memanggil controller staff
-require_once 'controllers/StaffController.php';
